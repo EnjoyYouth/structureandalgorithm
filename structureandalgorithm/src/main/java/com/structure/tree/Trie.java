a01095cf770d1076c97214cbf38497c68d346efe
wangsg3 <qwe123!@#>
2020-10-10 18:30:29 +0800

    【数据结构】【前缀树】支持泛型、提供获取最长前缀的功能

--- structureandalgorithm/src/main/java/com/structure/tree/Trie.java
+++ structureandalgorithm/src/main/java/com/structure/tree/Trie.java
@@ -9,28 +9,28 @@ import java.util.Map;
  * @author wangsg3
  *
  */
-public class Trie {
+public class Trie<V> {
 
 	/**
 	 * 前缀树根节点
 	 */
-	private TrieNode rootNode;
+	private TrieNode<V> rootNode;
 
 	/**
 	 * 初始化数据
 	 */
 	public Trie() {
 		super();
-		rootNode = new TrieNode();
+		rootNode = new TrieNode<V>();
 	}
 
 	/** Inserts a word into the trie. */
-	public void insert(String word) {
+	public void insert(String word, V value) {
 		if (word == null || word.isEmpty()) {
-			return;
+			throw new NullPointerException();
 		}
 
-		TrieNode node = this.rootNode;
+		TrieNode<V> node = this.rootNode;
 		for (int i = 0; i < word.length(); i++) {
 			char key = word.charAt(i);
 //			包含该字符时则继续深入子节点
@@ -39,24 +39,25 @@ public class Trie {
 			}
 //			节点不包含该字符时则插入
 			else {
-				node.childMap.put(key, new TrieNode());
+				node.childMap.put(key, new TrieNode<V>());
 				node = node.childMap.get(key);
 			}
 
-//			最后一个字符所在节点的记录为完整字符串
-			if (i == word.length() - 1 && !node.isWorld) {
+//			最后一个字符所在节点的记录为完整字符串，如先前已存在则更新数据
+			if (i == word.length() - 1) {
 				node.isWorld = true;
+				node.data = value;
 			}
 		}
 	}
 
 	/** Returns if the word is in the trie. */
-	public boolean search(String word) {
+	public boolean contains(String word) {
 		if (word == null || word.isEmpty()) {
 			return false;
 		}
 
-		TrieNode node = this.rootNode;
+		TrieNode<V> node = this.rootNode;
 		for (int i = 0; i < word.length(); i++) {
 			char key = word.charAt(i);
 			if (!node.childMap.containsKey(key)) {
@@ -72,12 +73,12 @@ public class Trie {
 	/**
 	 * Returns if there is any word in the trie that starts with the given prefix.
 	 */
-	public boolean startsWith(String prefix) {
+	public boolean containsPrefix(String prefix) {
 		if (prefix == null || prefix.isEmpty()) {
 			return false;
 		}
 
-		TrieNode node = this.rootNode;
+		TrieNode<V> node = this.rootNode;
 		for (int i = 0; i < prefix.length(); i++) {
 			char key = prefix.charAt(i);
 			if (!node.childMap.containsKey(key)) {
@@ -86,8 +87,51 @@ public class Trie {
 				node = node.childMap.get(key);
 			}
 		}
-
 		return true;
+	}
+	
+	/**
+	 * 获取键所对应的值
+	 * @param key	键
+	 * @return	键所对应的数据
+	 */
+	public V get(String key) {
+		if (key == null || key.isEmpty()) {
+			throw new NullPointerException();
+		}
+
+		TrieNode<V> node = this.rootNode;
+		for (int i = 0; i < key.length(); i++) {
+			char ch = key.charAt(i);
+			if (!node.childMap.containsKey(ch)) {
+				return null;
+			} else {
+				node = node.childMap.get(ch);
+			}
+		}
+
+		return node.data;
+	}
+	
+	/**
+	 * 返回key在前缀树中的最长前缀
+	 * @param key
+	 * @return
+	 */
+	public String getLongestPrefix(String key) {
+		if (key == null || key.isEmpty()) {
+			throw new NullPointerException();
+		}
+		
+		TrieNode<V> node = this.rootNode;
+		int i = 0;
+		for (; i < key.length(); i++) {
+			char ch = key.charAt(i);
+			if (!node.childMap.containsKey(ch)) {
+				break;
+			}
+		}
+		return key.substring(0, i);
 	}
 	
 	/**
@@ -96,12 +140,15 @@ public class Trie {
 	 * @author wangsg3
 	 *
 	 */
-	class TrieNode {
+	class TrieNode<N> {
 //		是否为完整单词
 		boolean isWorld = false;
+		
+//		节点中缓存的数据
+		N data;
 
 //		该节点对应的子节点
-		Map<Character, TrieNode> childMap = new HashMap<Character, Trie.TrieNode>();
+		Map<Character, TrieNode<N>> childMap = new HashMap<Character, TrieNode<N>>();
 	}
 
 }
